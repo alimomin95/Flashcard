b81c6e3a191a238532f546e2336647826b726a53
Ali Ziyaan Momin <alimomin@utexas.edu>
2016-07-30 13:57:54 -0500

    Alpha Program

--- program/program.py
+++ program/program.py
@@ -8,7 +8,6 @@ def fileLength(fname):
 	return i+1
 
 def getQuestion(fname, line):
-	#print line
 	with open(fname) as f:
 		reading = ''
 		for i in range(0,line):
@@ -37,18 +36,41 @@ def getAnswer(fname, line):
 				answer = answer + a
 	return answer
 
+def printStats(correct, incorrect, total):
+	print 'You got ' + str(correct) + ' correct answers.'
+	print 'You got ' + str(incorrect) + ' incorrect answers'
+	print 'Your accuracy was ' + str((float(correct)/float(total))*100.00) + '%.'
 
-filename = 'capitals.txt' #raw_input("Please enter the name of the file with flashcard:")
+correct = 0
+incorrect = 0
+total = 0
 
-length = fileLength(filename)
+while 1:
+	filename = 'capitals.txt' #raw_input("Please enter the name of the file with flashcard:")
 
-linenumber = randint(1, int(length))
+	length = fileLength(filename)
+	total = correct + incorrect
 
-question = getQuestion(filename, linenumber)
-answer = getAnswer(filename, linenumber)
+	linenumber = randint(1, int(length))
 
-print question
-print answer
+	question = getQuestion(filename, linenumber)
+	answer = getAnswer(filename, linenumber)
+
+	user_input = raw_input('What is the capital of ' + question + ': ')
+
+	if user_input == 'exit':
+		printStats(correct, incorrect, total)
+		print 'Goodbye!'
+		break
+
+	if user_input == answer:
+		correct += 1
+		print 'Correct!'
+	else:
+		incorrect += 1
+		print 'Incorrect! The capital of ' + question + ' is ' + answer + '.'
+
+
 
 
 
